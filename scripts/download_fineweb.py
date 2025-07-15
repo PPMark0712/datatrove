06277168ebf3e3d08a01497eea8ab02b47d043cb
939051420 <939051420@qq.com>
2025-07-15 15:31:01 +0800

    add download fineweb

---
+++ scripts/download_fineweb.py
@@ -0,0 +1,27 @@
+# HF_ENDPOINT="https://hf-mirror.com" python download_fineweb.py
+import os
+from datatrove.executor import LocalPipelineExecutor
+from datatrove.pipeline.readers import ParquetReader
+from datatrove.pipeline.writers import JsonlWriter
+
+if __name__ == "__main__":
+    base_output_path = "/data1/yyz/downloads/datasets/fineweb"
+    data_path = os.path.join(base_output_path, "data")
+    log_path = os.path.join(base_output_path, "logs")
+
+    pipeline_exec = LocalPipelineExecutor(
+        pipeline=[
+            ParquetReader(
+                "hf://datasets/HuggingFaceFW/fineweb/data",
+                limit=100000
+            ),
+            JsonlWriter(
+                data_path,
+                compression=None
+            )
+        ],
+        tasks=32,
+        workers=1,
+        logging_dir=log_path
+    )
+    pipeline_exec.run()
