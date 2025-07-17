e168438d256eb96f0401983d02a2a4c942f23148
939051420 <939051420@qq.com>
2025-07-17 20:35:37 +0800

    update lex dif

--- src/datatrove/pipeline/cl_wordnet/lexical_difficulty_calculator.py
+++ src/datatrove/pipeline/cl_wordnet/lexical_difficulty_calculator.py
@@ -1,112 +1,77 @@
+import nltk
+from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tag import pos_tag
+from nltk.wsd import lesk
+
 from datatrove.data import DocumentsPipeline
 from datatrove.pipeline.base import PipelineStep
-from datatrove.pipeline.cl_wordnet.sam import SAM
-from datatrove.pipeline.cl_wordnet.word_extractor import WordExtractor
 from datatrove.utils.logging import logger
 
-class LexicalDifficultyLabeler(PipelineStep):
-    name = "🔤 - Lexical difficulty labeler"
+def get_wordnet_pos(nltk_pos_tag:str):
+    if nltk_pos_tag.startswith('J'):
+        return wn.ADJ
+    elif nltk_pos_tag.startswith('V'):
+        return wn.VERB
+    elif nltk_pos_tag.startswith('N'):
+        return wn.NOUN
+    elif nltk_pos_tag.startswith('R'):
+        return wn.ADV
+    else:
+        return None
+
+
+class LexicalDifficultyCalculator(PipelineStep):
+    name = "🔤 - Lexical difficulty calculator"
     type = "cl"
-    
+
     def __init__(
         self,
-        dict_files: dict[str, str],
-        language: str="zh",
-        use_sam=False,
+        **kwargs
     ):
         super().__init__()
-        self.dict_files = dict_files
-        self.language = language
-        self.use_sam = use_sam
-
-    def get_zh_chars(self, text: str) -> str:
-        return "".join([c if "\u4e00" <= c <= "\u9fff" else " " for c in text])
+        nltk_dependencies = [
+            "stopwords"
+        ]
+        if "nltk_path" in kwargs:
+            nltk.data.path.append(kwargs["nltk_path"])
+        for package in nltk_dependencies:
+            try:
+                nltk.data.load(package)
+            except:
+                nltk.download(package, download_dir=kwargs.get("nltk_path", None))
+        self.stop_words = stopwords
+    
+    def is_valid_word(self, word: str) -> bool:
+        return len(word) > 1 and any(c.isalpha() for c in word) and word not in self.stop_words
 
     def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1):
-        if self.use_sam:
-            with self.track_time():
-                level_sam_dict = {}
-                sam_for_all_words = SAM()
-                for level, dict_file in self.dict_files.items():
-                    with open(dict_file, "r") as f:
-                        words = f.read().splitlines()
-                        for word in words:
-                            sam_for_all_words.add_string(word)
-                        level_sam = SAM()
-                        for word in words:
-                            level_sam.add_string(word)
-                        level_sam_dict[level] = level_sam
-
-            with self.track_time():
-                for doc in data:
-                    level_word_counter = {}
-                    zh_chars = self.get_zh_chars(doc.text)
-                    zh_splits = zh_chars.split(" ")
-                    
-                    def match_exist(sam, s):
-                        p = 0
-                        cnt = 0
-                        for i, c in enumerate(s):
-                            p = sam.trans_keep_suffix(p, c)
-                            if sam.exist[p]:
-                                cnt += 1
-                            # logger.info(f"{''.join(s[max(0, i - 2): i + 1])}, p={p}, len={sam.len[p]}")
-                        return cnt
-
-                    def match_not_exist(sam, s):
-                        p = 0
-                        matched_suf_len = []
-                        for c in s:
-                            p = sam.trans_keep_suffix(p, c)
-                            q = p
-                            while q > 0 and not sam.exist[q]:
-                                q = sam.fa[q]
-                            matched_suf_len.append(sam.len[q])
-                        cnt = 0
-                        for i in range(len(matched_suf_len) - 3):
-                            if matched_suf_len[i] >= matched_suf_len[i + 1] >= matched_suf_len[i + 2] >= matched_suf_len[i + 3]:
-                                logger.info(f"{s[i:i+4]}, {matched_suf_len[i:i+4]}")
-                                cnt += 1
-                        return cnt
-                        # last_matched_idx = -1
-                        # p = 0
-                        # cnt = 0
-                        # for i, c in enumerate(s):
-                        #     p = sam.trans_keep_suffix(p, c)
-                        #     if sam.exist[p]:
-                        #         if i - last_matched_idx > sam.len[p]:
-                        #             cnt += i - sam.len[p] - last_matched_idx
-                        #             logger.info(f"{''.join(s[max(0, i - 5): i + 1])}, p={p}, len={sam.len[p]}, last={s[last_matched_idx - sam.len[last_matched_idx] + 1: last_matched_idx + 1] if last_matched_idx != -1 else ''}")
-                        #         last_matched_idx = i
-                        return cnt
-
-                    for level, level_sam in level_sam_dict.items():
-                        level_word_counter[level] = 0
-                        for split in zh_splits:
-                            level_word_counter[level] += match_exist(level_sam, split)
-                    level_word_counter["other"] = 0
-                    for split in zh_splits:
-                        level_word_counter["other"] += match_not_exist(sam_for_all_words, split)
-                    doc.metadata["level_word_counter"] = level_word_counter
-                    yield doc
-        else:
-            level_word_set = {}
-            for level, dict_file in self.dict_files.items():
-                with open(dict_file, "r") as f:
-                    words = f.read().splitlines()
-                level_word_set[level] = set(words)
-            word_extractor = WordExtractor(self.language)
+        with self.track_time():
             for doc in data:
-                level_word_counter = {}
-                zh_chars = self.get_zh_chars(doc.text)
-                words = set(word_extractor.extract_words(zh_chars))
-                for level, word_set in level_word_set.items():
-                    level_word_counter[level] = len(word_set.intersection(words))
-                    # if level != "primary":
-                    #     logger.info(f"{level}: {word_set.intersection(words)}")
-                level_word_counter["other"] = len(words) - sum(level_word_counter.values())
-                doc.metadata["level_word_counter"] = level_word_counter
-                yield doc
+                text = doc.text
+                sentences = sent_tokenize(text)
+                total_words_counted = 0
+                sum_hypernym_depth = 0
+                total_synsets_count = 0
+                abstract_word_count = 0
+                for sentence in sentences:
+                    words = word_tokenize(sentence)
+                    words_with_pos = pos_tag(words)
+                    for word, pos in words_with_pos:
+                        if not self.is_valid_word(word):
+                            continue
+                        total_words_counted += 1
+
+                        synsets = wn.synsets(word)
+                        total_synsets_count += len(synsets)
+                        wn_pos = get_wordnet_pos(pos)
+                        synset = lesk(words, word, pos=wn_pos)
+                        if synset is not None:
+                            path = synset.hypernyms()
+                            sum_hypernym_depth += len(path)
+                            total_calculated_words += 1
+
 
 
 class LexicalDifficultySorter(PipelineStep):
@@ -119,6 +84,7 @@ class LexicalDifficultySorter(PipelineStep):
     ):
         super().__init__()
         self.weights = weights
+        self.stop_words = stop
 
     def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1):
         with self.track_time():
