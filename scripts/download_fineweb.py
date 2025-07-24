fd87b8c86da359cbe4f46509e2c2b93180389814
939051420 <939051420@qq.com>
2025-07-24 22:08:36 +0800

    fix download_fineweb

--- scripts/download_fineweb.py
+++ scripts/download_fineweb.py
@@ -1,11 +1,12 @@
 # HF_ENDPOINT="https://hf-mirror.com" python download_fineweb.py
 import os
+import time
 from datatrove.executor import LocalPipelineExecutor
 from datatrove.pipeline.readers import ParquetReader
 from datatrove.pipeline.writers import JsonlWriter
 
 if __name__ == "__main__":
-    base_output_path = "/data1/yyz/downloads/datasets/fineweb"
+    base_output_path = "/data1/yyz/downloads/datasets/fineweb_2e7"
     data_path = os.path.join(base_output_path, "data")
     log_path = os.path.join(base_output_path, "logs")
 
@@ -13,15 +14,22 @@ if __name__ == "__main__":
         pipeline=[
             ParquetReader(
                 "hf://datasets/HuggingFaceFW/fineweb/data",
-                limit=100000
+                limit=300000
             ),
             JsonlWriter(
                 data_path,
                 compression=None
             )
         ],
-        tasks=32,
-        workers=4,
+        tasks=64,
+        workers=8,
         logging_dir=log_path
     )
-    pipeline_exec.run()
+    while True:
+        try:
+            stats = pipeline_exec.run()
+            if stats is None:
+                break
+        except Exception as e:
+            print(e)
+            time.sleep(60)
