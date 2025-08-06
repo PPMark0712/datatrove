35df8e504de867e2b987328545c2d8e9afd97d27
939051420 <939051420@qq.com>
2025-08-06 16:59:33 +0800

    complete

--- scripts/cl_wordnet/run.sh
+++ scripts/cl_wordnet/run.sh
@@ -1,5 +1,5 @@
 python sort_by_lexical_difficulty.py \
-    --input_path /data1/yyz/downloads/datasets/fineweb_1e7/data \
-    --output_path /data1/yyz/data/datatrove_output/fineweb_1e7_wordnet \
+    --input_path /data1/yyz/downloads/datasets/fineweb_edu_1e7/data \
+    --output_path /data1/yyz/data/datatrove_output/fineweb_edu_1e7_wordnet \
     --tasks 32 \
     --workers 32
---
+++ src/datatrove/pipeline/writers/bin.py
@@ -0,0 +1,41 @@
+from typing import IO, Callable
+
+from datatrove.io import DataFolderLike
+from datatrove.pipeline.writers.disk_base import DiskWriter
+
+
+class BinWriter(DiskWriter):
+    """Write data to datafolder (local or remote) in BIN format
+
+    Args:
+        output_folder: a str, tuple or DataFolder where data should be saved
+        output_filename: the filename to use when saving data, including extension. Can contain placeholders such as `${rank}` or metadata tags `${tag}`
+        adapter: a custom function to "adapt" the Document format to the desired output format
+        expand_metadata: save each metadata entry in a different column instead of as a dictionary
+    """
+
+    default_output_filename: str = "${rank}.bin"
+    name = "🐿 Bin"
+    _requires_dependencies = ["orjson"]
+
+    def __init__(
+        self,
+        output_folder: DataFolderLike,
+        output_filename: str = None,
+        adapter: Callable = None,
+        expand_metadata: bool = False
+    ):
+        super().__init__(
+            output_folder,
+            output_filename=output_filename,
+            compression=None,
+            adapter=adapter,
+            expand_metadata=expand_metadata,
+            mode="wb",
+            max_file_size=max_file_size,
+        )
+
+    def _write(self, document: dict, file_handler: IO, _filename: str):
+        import orjson
+
+        file_handler.write(orjson.dumps(document, option=orjson.OPT_APPEND_NEWLINE))
