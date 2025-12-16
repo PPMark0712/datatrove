c05b5cd3e2a354b8e8dcf5d369118712ce95b57a
PPMark0712 <939051420@qq.com>
2025-12-16 07:24:20 +0000

    fix frequency difficulty

--- src/datatrove/pipeline/freq_conc/freq_conc_difficulty.py
+++ src/datatrove/pipeline/freq_conc/freq_conc_difficulty.py
@@ -22,7 +22,7 @@ def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
 
-def calc_freq_difficulty(log_freq, freq_scaling_factor=0.8, log_freq_center=math.log(2233306)):
+def calc_freq_difficulty(log_freq, freq_scaling_factor, log_freq_center):
     return 1 - sigmoid(freq_scaling_factor * (log_freq - log_freq_center))
 
 
@@ -32,13 +32,15 @@ def power_mean(scores: list, alpha=1.5):
     return (sum([s ** alpha for s in scores]) / len(scores)) ** (1 / alpha)
 
 
-def merge_score(scores: list, alpha=1.5, top_p=0.1, top_weight=0.7):
+def agg_scores(scores: list, alpha=1.5, top_quantile=0.9, top_weight=0.7):
     """merge list of one word difficulty into paragraph difficulty"""
-    scores.sort(reverse=True)
-    top_k = int(len(scores) * top_p)
-    top_k_scores = scores[:top_k]
-    other_scores = scores[top_k:]
-    return power_mean(top_k_scores, alpha) * top_weight + power_mean(other_scores, alpha) * (1 - top_weight)
+    if len(scores) <= 20:
+        return power_mean(scores, alpha)
+    scores.sort()
+    split_id = int(len(scores) * top_quantile)
+    top_scores = scores[split_id:]
+    other_scores = scores[:split_id]
+    return power_mean(top_scores, alpha) * top_weight + power_mean(other_scores, alpha) * (1 - top_weight)
 
 
 class FcdCalculator(PipelineStep):
@@ -48,14 +50,14 @@ class FcdCalculator(PipelineStep):
     def __init__(
         self,
         output_folder: DataFolderLike,
-        freq_scaling_factor=0.8,
-        freq_center_rate=0.001,
+        freq_scaling_factor=0.7,
+        log_freq_center=math.log(36597166),
         power_mean_alpha=1.5,
-        merge_top_p=0.1,
-        merge_top_weight=0.6,
-        noun_weight=0.6,
+        agg_top_quantile=0.9,
+        agg_top_weight=0.7,
+        noun_weight=0.7,
         dis_to_basic_path = "/mnt/data/kw/yyz/projects/CurriculumLearning/build_dict/output/dict/dis_to_basic.txt",
-        word_freq_path = "/mnt/data/kw/yyz/projects/CurriculumLearning/build_dict/output/dict/word_count.txt",
+        word_freq_path = "/mnt/data/kw/yyz/projects/CurriculumLearning/build_dict/output/dict/word_freq.txt",
         **kwargs
     ):
         super().__init__()
@@ -66,13 +68,14 @@ class FcdCalculator(PipelineStep):
         self.stop_words = set(stopwords.words("english"))
 
         # logger.info("loading dis to basic")
-        self.dis_to_difficulty = {i: math.log(i + 1) / math.log(11) for i in range(11)}
         self.dis_to_basic = {}
 
         with open(dis_to_basic_path, "r") as f:
             for line in f:
                 synset_name, dis = line.strip().split(" ")
                 self.dis_to_basic[synset_name] = int(dis)
+        max_dis = max(self.dis_to_basic.values())
+        self.dis_to_difficulty = {i: math.log(i + 1) / math.log(max_dis + 1) for i in range(max_dis + 1)}
 
         # logger.info("loading word freq")
         self.word_log_freq = {}
@@ -84,15 +87,12 @@ class FcdCalculator(PipelineStep):
 
         self.freq_scaling_factor = freq_scaling_factor
         self.power_mean_alpha = power_mean_alpha
-        self.merge_top_p = merge_top_p
-        self.merge_top_weight = merge_top_weight
+        self.agg_top_quantile = agg_top_quantile
+        self.agg_top_weight = agg_top_weight
         self.noun_weight = noun_weight
 
         # logger.info("calculating freq center")
-        total_words = len(self.word_log_freq)
-        freqs = list(self.word_log_freq.values())  # already sorted (descending)
-        sigmoid_center_id = int(total_words * freq_center_rate)
-        self.log_freq_center = freqs[sigmoid_center_id]
+        self.log_freq_center = log_freq_center
         logger.info("initialized")
 
     def is_valid_word(self, word: str) -> bool:
@@ -101,6 +101,8 @@ class FcdCalculator(PipelineStep):
         if word in self.stop_words:
             return False
         if not word.isalpha():
+            return False
+        if word.lower() not in self.word_log_freq:
             return False
         return True
 
@@ -126,14 +128,15 @@ class FcdCalculator(PipelineStep):
                 if synset and synset.name() in self.dis_to_basic:
                     concept_dis = self.dis_to_basic[synset.name()]
                 else:
-                    concept_dis = 10
+                    concept_dis = 2
                 concept_difficulty = self.dis_to_difficulty[concept_dis]
-                freq_difficulty = calc_freq_difficulty(self.word_log_freq.get(word.lower(), 0))
+                freq_difficulty = calc_freq_difficulty(self.word_log_freq.get(word.lower(), 0), self.freq_scaling_factor, self.log_freq_center)
                 score = (concept_difficulty * freq_difficulty) ** 0.5
-                # score = freq_difficulty
+                # score = freq_difficulty  # ablation
+                # score = concept_difficulty # ablation
                 noun_scores.append((score, word))
             else:
-                freq_difficulty = calc_freq_difficulty(self.word_log_freq.get(word.lower(), 0))
+                freq_difficulty = calc_freq_difficulty(self.word_log_freq.get(word.lower(), 0), self.freq_scaling_factor, self.log_freq_center)
                 score = freq_difficulty
                 non_noun_scores.append((score, word))
         return noun_scores, non_noun_scores
@@ -154,8 +157,8 @@ class FcdCalculator(PipelineStep):
 
                 noun_scores = [score for score, word in noun_scores_with_words]
                 non_noun_scores = [score for score, word in non_noun_scores_with_words]
-                noun_difficulty = merge_score(noun_scores)
-                non_noun_difficulty = merge_score(non_noun_scores)
+                noun_difficulty = agg_scores(noun_scores, self.power_mean_alpha, self.agg_top_quantile, self.agg_top_weight)
+                non_noun_difficulty = agg_scores(non_noun_scores, self.power_mean_alpha, self.agg_top_quantile, self.agg_top_weight)
                 difficulty = self.noun_weight * noun_difficulty + (1 - self.noun_weight) * non_noun_difficulty
                 difficulty_list.append(difficulty)
             with self.output_folder.open(f"{rank:05d}.json", mode="w") as f:
